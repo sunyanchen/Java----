915bd180aebb84a82ff1cd0909d05b38eedb5d89
sunyanchen <qingyiwangluo123@163.com>
2016-09-06 18:52:43 +0800

    login

---
+++ ServletLoginDemo/WebContent/login_success.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>登录成功</title>
+<link type="text/css" href="css/login.css" rel="stylesheet" />	
+		<link type="text/css" href="css/smoothness/jquery-ui-1.7.2.custom.html" rel="stylesheet" />	
+		<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+		<script type="text/javascript" src="js/easyTooltip.js"></script>
+		<script type="text/javascript" src="js/jquery-ui-1.7.2.custom.min.js"></script>
+</head>
+<body>
+<div id="container">
+		<div class="logo">
+			<a href="#"><img src="assets/logo.png" alt="" /></a>
+		</div>
+		<div id="box">
+		  <% 
+		     String loginUser = "";
+		     if(session.getAttribute("loginuser")!=null)
+		     {
+		        loginUser = session.getAttribute("loginuser").toString();
+		     }
+		  %>
+		     欢迎您<font color="red"><%=loginUser%></font>,登录成功！
+		</div>
+	</div>
+
+</body>
+</html>
---
+++ ServletLoginDemo/src/servlet/LoginServlet.java
@@ -0,0 +1,69 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entity.Users;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet(name="LoginServlet",urlPatterns="/servlet/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		
+		Users u = new Users();
+		u.setUsername(request.getParameter("username"));
+		u.setPassword(request.getParameter("password"));
+		if (u.getUsername().equals("admin") && u.getPassword().equals("admin")) {
+			response.sendRedirect(request.getContextPath() + "/login_success.jsp");
+		}
+		else {
+			response.sendRedirect(request.getContextPath() + "/login_failure.jsp");
+		}
+	}
+
+}
